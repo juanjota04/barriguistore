3797359db4f5e6518e9876825f7e316ad6595058
juanjota04 <jjgelvez04_92@hotmail.com>
2022-12-29 11:18:43 -0500

    PageVercel

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['api.lorem.space', 'placeimg.com', 'wixmp.com', 'romapy'],
+  },
+  // env: {
+  //   customKey: 'customValue',
+  // },
+  // basePath: '/dist',
+  // compress: true,
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: '/hola', 
+  //       destination: '/hello',
+  //       permanent: true,
+  //     }
+  //   ]
+  // }
 }
 
 module.exports = nextConfig
